0e8fec8ddde004177ed6343d37dc06e53333131d
Balisa16 <sanderantoniusbalisa16@gmail.com>
2024-04-20 22:44:42 +0700

    Update

--- main2.py
+++ main2.py
@@ -3,17 +3,13 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
 
-filename = 'docs/way.csv'
+filename = 'docs/track.csv'
 data = []
 header = "None"
 try:
     with open(filename, 'r') as file:
         csv_reader = csv.reader(file)
-        
-        # Read and print headers
         header = next(csv_reader)
-        
-        # Read and print data
         for row in csv_reader:
             data.append(row)
 except FileNotFoundError:
@@ -30,12 +26,12 @@ target_y = []
 target_z = []
 for row in data:
     label.append(row[0])
-    sim_x.append(float(row[2]))
-    sim_y.append(float(row[3]))
-    sim_z.append(float(row[4]))
-    target_x.append(float(row[5]))
-    target_y.append(float(row[6]))
-    target_z.append(float(row[7]))
+    sim_x.append(float(row[3]))
+    sim_y.append(float(row[4]))
+    sim_z.append(float(row[5]))
+    target_x.append(float(row[10]))
+    target_y.append(float(row[11]))
+    target_z.append(float(row[12]))
 
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
@@ -43,7 +39,7 @@ ax = fig.add_subplot(111, projection='3d')
 ax.plot(sim_x, sim_y, sim_z, marker='o')
 ax.plot(target_x, target_y, target_z, marker='o')
 mng = plt.get_current_fig_manager()
-mng.window.state('zoomed') 
+mng.resize(*mng.window.maxsize())
 plt.axis('equal')
 
 ax.set_xlabel('X Axis')
